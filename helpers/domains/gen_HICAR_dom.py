5eb46163279b37b2ced44e945f6a791076bdabf5
d3sp3r4t3Student <max.sesselmann.m.s@gmail.com>
2025-03-19 12:35:45 +0100

    domain stuff

--- helpers/domains/gen_HICAR_dom.py
+++ helpers/domains/gen_HICAR_dom.py
@@ -9,18 +9,18 @@ import HICAR_Domain as hd
 ###########################################################################
 
 #The horizontal resolution of the domain in meters
-res = 50
+res = 10
 
 # The target domain, including lat and lon variables named as "lat" and "lon", and
 # a DEM labeled as "topo". Optionally, landuse and landmask variables should be specified here.
-target_domain_fn = '/mnt/c/Users/sesselma/Code/Code_PhD/HICAR/domains/50m_domains/Silvretta_50m_coarse.nc'
+target_domain_fn = '/mnt/c/Users/sesselma/Code/Code_PhD/HICAR/domains/10m_domains/WGS84_domain_swissaltiregio10m_HEF_wlu_small.nc'
 # A domain with extent ~20km beyond the borders of the above target domain. 
 # Only lat,lon, and topo are required variables.
-large_domain_fn = '/mnt/c/Users/sesselma/Code/Code_PhD/HICAR/domains/50m_domains/Silvretta_50m_coarse_big.nc'
+large_domain_fn = '/mnt/c/Users/sesselma/Code/Code_PhD/HICAR/domains/10m_domains/WGS84_domain_swissaltiregio10m_HEF_wlu.nc'
 # Name of output file
-output_domain_fn = '/mnt/c/Users/sesselma/Code/Code_PhD/HICAR/domains/50m_domains/Silvretta_50m_final.nc'
+output_domain_fn = '/mnt/c/Users/sesselma/Code/Code_PhD/HICAR/domains/10m_domains/Hintereis_10m_final.nc'
 
-# These are used in the calculation of ridelines, and can be tuned if the user
+# These are used in the calculation of ridgelines, and can be tuned if the user
 # is not satisfied with the deliniation of ridgelines in the output file
 # terr_filter = 10
 # TPI_thresh = 100
